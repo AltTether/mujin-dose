6ba5e1b472cb3314f6406a0b012ae96e925a0d71
AltTether <200351p@ugs.kochi-tech.ac.jp>
2019-01-27 13:53:27 +0900

    ログとコメント追加

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import json
 import requests
 
-from logging import getLogger
+from src import Logger
 
 
 HOST_KEYS = ['FACE_HOST', 'ARUCO_HOST', 'MAGNET_HOST']
@@ -13,6 +13,8 @@ CONFIG = {'PROTOCOL_BASE': 'http://',
           'ARUCO_HOST': 'aruco', 'ARUCO_PORT': '5000',
           'MAGNET_HOST': 'magnet', 'MAGNET_PORT': '5000'}
 
+logger = Logger()
+
 def main():
     set_CONFIG()
 
@@ -20,13 +22,19 @@ def main():
     aruco_url = build_url(CONFIG['ARUCO_HOST'], CONFIG['ARUCO_PORT'])
     magnet_url = build_url(CONFIG['MAGNET_HOST'], CONFIG['MAGNET_PORT'])
 
+    logger.info('mujin service is starting')
     while(True):
+        logger.info('mujin service start to check the door')
+
+        # 扉開閉検知をする
         response = requests.get(magnet_url, stream=True)
         for line in response.iter_lines():
             if line:
                 response_json = json.loads(line.decode('utf-8'))
-                if response_json['state'] == 1:
-                    print('open')
+                if response_json['state'] == 0:
+                    break
+        logger.info('mujin item case\'s door is opened')
+
 
 def build_url(host, port):
     return CONFIG['PROTOCOL_BASE'] + host + ':' + port
