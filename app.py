dd6133a3096bcc21f63d63c46fc1d78fb5e72c1a
AltTether <200351p@ugs.kochi-tech.ac.jp>
2019-01-27 13:41:13 +0900

    環境変数からのホスト名とポートの受け渡しを優先

--- app.py
+++ app.py
@@ -1,33 +1,45 @@
+import os
+import json
 import requests
 
+from logging import getLogger
 
-PROTOCOL_BASE = "http://"
 
-FACE_HOSTNAME = "face"
-FACE_PORT = "5000"
+HOST_KEYS = ['FACE_HOST', 'ARUCO_HOST', 'MAGNET_HOST']
+PORT_KEYS = ['FACE_PORT', 'ARUCO_PORT', 'MAGNET_PORT']
 
-ARUCO_HOSTNAME = "aruco"
-ARUCO_PORT = "5000"
-
-MAGNET_PORT = "5000"
-MAGNET_HOSTNAME = "magnet"
-
+CONFIG = {'PROTOCOL_BASE': 'http://',
+          'FACE_HOST': 'face', 'FACE_PORT': '5000',
+          'ARUCO_HOST': 'aruco', 'ARUCO_PORT': '5000',
+          'MAGNET_HOST': 'magnet', 'MAGNET_PORT': '5000'}
 
 def main():
-    face_url = build_url(FACE_HOSTNAME, FACE_PORT)
-    aruco_url = build_url(ARUCO_HOSTNAME, ARUCO_PORT)
-    magnet_url = build_url(MAGNET_HOSTNAME, MAGNET_PORT)
+    set_CONFIG()
+
+    face_url = build_url(CONFIG['FACE_HOST'], CONFIG['FACE_PORT'])
+    aruco_url = build_url(CONFIG['ARUCO_HOST'], CONFIG['ARUCO_PORT'])
+    magnet_url = build_url(CONFIG['MAGNET_HOST'], CONFIG['MAGNET_PORT'])
 
     while(True):
         response = requests.get(magnet_url, stream=True)
         for line in response.iter_lines():
             if line:
-                response_json = line.json()
-                if response_json["state"] == 1:
-                    print("open")
+                response_json = json.loads(line.decode('utf-8'))
+                if response_json['state'] == 1:
+                    print('open')
 
 def build_url(host, port):
-    return PROTOCL_BASE + host + ":" + port
+    return CONFIG['PROTOCOL_BASE'] + host + ':' + port
+
+def set_CONFIG():
+    for host in HOST_KEYS:
+        if os.getenv(host):
+            CONFIG[host] = os.environ[host]
+
+    for port in PORT_KEYS:
+        if os.getenv(port):
+            CONFIG[port] = os.environ[port]
+
 
 
 if __name__ == '__main__':
